939568a6af5c6aa5afee7c47d89a4ecbe215efb5
ducndh <nguyend@carleton.edu>
2024-03-10 11:56:29 +0700

    dynamic html template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"strconv"
+	"text/template"
 
 	"snippetbox.letgoducndh.net/internal/models"
 )
@@ -50,23 +51,40 @@ func (app *application) curse(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) view(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil || id < 1 {
 		app.notFound(w)
 		return
-	}
-
-	snippet, err := app.snippets.Get(id)
-	if err != nil {
+		}
+		snippet, err := app.snippets.Get(id)
+		if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
+		app.notFound(w)
 		} else {
-			app.serverError(w, err)
+		app.serverError(w, err)
 		}
 		return
-	}
-	fmt.Fprintf(w, "Here is your %+v", snippet)
-}
+		}
+		// Initialize a slice containing the paths to the view.tmpl file,
+		// plus the base layout and navigation partial that we made earlier.
+		files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+		}
+		// Parse the template files...
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+		app.serverError(w, err)
+		return
+		}
+		// And then execute them. Notice how we are passing in the snippet
+		// data (a models.Snippet struct) as the final parameter?
+		err = ts.ExecuteTemplate(w, "base", snippet)
+		if err != nil {
+		app.serverError(w, err)
+		}
+		}
 
 func (app *application) create(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
