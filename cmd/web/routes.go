4be0b2bd5c979f6a4f8f70cced8d753f632a7e56
ducndh <ducndh@gmail.com>
2024-03-14 17:53:38 +0700

    using context to check if user is actually log in or not

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes(cfg config) http.Handler {
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("/", dynamic.ThenFunc(app.home))
 	mux.Handle("/curse", dynamic.ThenFunc(app.curse))
