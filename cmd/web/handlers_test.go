7b79eac22ff7a4d3db6ecb942950754d02bbabda
ducndh <nguyend@carleton.edu>
2024-03-27 23:47:00 +0700

    add test for creating snippet

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -74,6 +74,33 @@ func TestSnippetView(t *testing.T) {
 	}
 }
 
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/create")
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+	t.Run("Authenticated", func(t *testing.T) {
+
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		code, _, body := ts.get(t, "/create")
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, "<form action='/create' method='POST'>")
+	})
+}
+
 func TestCRSFToken(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
